dae80f813deeb27a0604ca6c1687b4028833fc26
Dylan Canfield <cmwl1970@hotmail.com>
2023-12-06 22:55:59 -0800

    Added a 404 page reflecting current style of other pages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const logger = require('./logger'); // Winston logger
+const logger = require('./logger'); 
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -40,10 +41,17 @@ app.use((err, req, res, next) => {
   res.status(500).send('Internal Server Error');
 });
 
-// 404 Handler - Warning Logging
-app.use((req, res) => {
-  logger.warn(`404 - Not Found: ${req.originalUrl}`);
-  res.status(404).send('404: Page Not Found');
+// 404 handles requests that do not match files or routes
+app.use((req, res, next) => {
+  res.status(404);
+
+  // Respond with html page if the request accepts html, otherwise send text response
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'public', '404.html'));
+    return;
+  }
+
+  res.type('txt').send('404 - Not Found');
 });
 
 // Start the server
